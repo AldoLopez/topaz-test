7de2ffdb2f5c4fefa9dbd168bd7fbb6ac5e50ee8
Aldo Lopez <lopez.aldo.a@gmail.com>
2023-08-29 14:41:17 -0400

    update with sig code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Button,
+  Box,
+  Divider,
+  TextField,
+} from '@mui/material';
 import './App.css';
+import { listener, startTablet } from './topaz/sigweb';
+
+/* sdk
+   https://www.topazsystems.com/web.html
+   Dev Guide
+   https://www.topazsystems.com/Software/download/sigweb.pdf
+   installer
+   https://www.topazsystems.com/software/sigweb.exe
+*/
 
 function App() {
+  const [sigCaptured, setSigCaptured] = useState<any>(null);
+
+  listener();
+
+  const handleSig = () => {};
+
+  const handleSigCapture = (sig: any) => {
+    let image = new Image();
+    image.src = 'data:image/png;base64,' + sig;
+    setSigCaptured({ image, sig });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Card>
+          <CardHeader title='Capture Signature' />
+          <Divider />
+          <CardContent>
+            <Box p={5}>
+              <canvas id='cnv' width='500' height='100' />
+            </Box>
+            <form action='#' name='FORM1'>
+              {!sigCaptured ? (
+                <Button
+                  onClick={() => {
+                    startTablet(handleSigCapture);
+                  }}
+                >
+                  Sign
+                </Button>
+              ) : (
+                <Button
+                  onClick={() => {
+                    handleSig();
+                  }}
+                >
+                  Done
+                </Button>
+              )}
+              <Typography variant='body1'>
+                Please have customer sign signature pad.
+              </Typography>
+              <Box p={2}>
+                <Typography>SigString:</Typography>
+                <TextField name='sigString' rows='20' multiline />
+              </Box>
+              <Box p={2}>
+                <Typography>ImgData:</Typography>
+                <TextField
+                  name='imgData'
+                  rows='20'
+                  multiline
+                  value={sigCaptured?.image.src}
+                />
+              </Box>
+              <Box p={2}>
+                <Typography>Image:</Typography>
+                <img src={sigCaptured?.image.src} />
+              </Box>
+            </form>
+          </CardContent>
+        </Card>
       </header>
     </div>
   );
